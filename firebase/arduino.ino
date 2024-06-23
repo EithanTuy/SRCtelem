6e6252dc850743d60f2d26dcf734974b46fa3560
EithanTuy <167654980+EithanTuy@users.noreply.github.com>
2024-06-23 16:21:44 -0400

    Update arduino.ino

--- firebase/arduino.ino
+++ firebase/arduino.ino
@@ -1,121 +1,85 @@
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-//UNFINISHED!!!!!!!!!!!!!!
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
-#include <SoftwareSerial.h>
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+//UNFINISED!!!!!!!!!!!
+
 #include <TinyGPS++.h>
-#include <TimeLib.h>   // Time library
-#include <WiFi.h>      // WiFi library
+#include <SoftwareSerial.h>
+#include <FirebaseESP8266.h>
+#include <ESP8266WiFi.h>
 
-SoftwareSerial esp8266(2, 3); // RX, TX
+// Replace these with your WiFi credentials and Firebase host/key
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+#define FIREBASE_HOST "your_firebase_database_url"
+#define FIREBASE_AUTH "your_firebase_database_secret"
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+// Initialize the TinyGPS++ library
 TinyGPSPlus gps;
+SoftwareSerial gpsSerial(4, 3); // RX, TX
 
-const char* ssid = "YourCarWiFiSSID";
-const char* password = "YourCarWiFiPassword";
-const char* host = "oksrctelem.netlify.app"; // Replace with your Netlify site hostname
-const int port = 80;
-
-WiFiClient client;
+// Set up Firebase
+FirebaseData firebaseData;
 
 void setup() {
+  // Start the serial communication for debugging
   Serial.begin(9600);
-  Wire.begin();
-  accel.begin();
-  esp8266.begin(9600);
-
+  // Start the serial communication with the GPS module
+  gpsSerial.begin(9600);
   // Connect to WiFi
-  connectWiFi();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Connect to Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  // Read accelerometer data
-  sensors_event_t event;
-  accel.getEvent(&event);
-  float ax = event.acceleration.x;
-  float ay = event.acceleration.y;
-  float az = event.acceleration.z;
-
-  // Read GPS data
-  while (Serial.available() > 0) {
-    if (gps.encode(Serial.read())) {
-      float latitude = gps.location.lat();
-      float longitude = gps.location.lng();
-      
-      // Get current time
-      String currentTime = getCurrentTime();
-
-      // Send data to ESP8266
-      sendSensorData(ax, ay, az, latitude, longitude, currentTime);
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+    
+    if (gps.location.isUpdated()) {
+      sendDataToFirebase();
     }
   }
-
-  delay(1000); // Adjust delay based on your requirements
 }
 
-String getCurrentTime() {
-  // Get current time
-  time_t now = time(nullptr);
-  struct tm * timeinfo;
-  timeinfo = localtime(&now);
-  char timeStr[20];
-  strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", timeinfo);
-  return String(timeStr);
-}
+void sendDataToFirebase() {
+  if (Firebase.ready()) {
+    String latitude = String(gps.location.lat(), 6);
+    String longitude = String(gps.location.lng(), 6);
+    String speed = String(gps.speed.kmph());
+    String direction = String(gps.course.deg());
 
-void sendSensorData(float ax, float ay, float az, float lat, float lon, String timeStr) {
-  String url = "/data?ax=" + String(ax) +
-               "&ay=" + String(ay) +
-               "&az=" + String(az) +
-               "&latitude=" + String(lat, 6) +
-               "&longitude=" + String(lon, 6) +
-               "&time=" + timeStr;
+    // Update the values in the Firebase database
+    Firebase.setString(firebaseData, "/carTelemetry/latitude", latitude);
+    Firebase.setString(firebaseData, "/carTelemetry/longitude", longitude);
+    Firebase.setString(firebaseData, "/carTelemetry/speed", speed);
+    Firebase.setString(firebaseData, "/carTelemetry/direction", direction);
 
-  if (client.connect(host, port)) {
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    delay(10); // Give the server some time to process
-
-    Serial.println("Sent data to server:");
-    Serial.println(url);
-
-    // Read and display response
-    while (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
-    Serial.println();
-  } else {
-    Serial.println("Failed to connect to server");
+    Serial.println("Data sent to Firebase:");
+    Serial.println("Latitude: " + latitude);
+    Serial.println("Longitude: " + longitude);
+    Serial.println("Speed: " + speed);
+    Serial.println("Direction: " + direction);
   }
-}
-
-void connectWiFi() {
-  esp8266.println("AT+RST");
-  delay(1000);
-  esp8266.println("AT+CWMODE=1");
-  delay(1000);
-  esp8266.println("AT+CWJAP=\"" + String(ssid) + "\",\"" + String(password) + "\"");
-  delay(3000);
 }
